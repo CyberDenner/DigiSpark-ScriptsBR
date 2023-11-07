354fbcb63d59f226d5feebc3fc856ac745a3a995
CyberDenner <146398551+CyberDenner@users.noreply.github.com>
2023-11-07 14:39:16 -0300

    Talker.ino

--- Talker/Talker.ino
+++ Talker/Talker.ino
@@ -1,33 +1,28 @@
-#include "DigiKeyboard.h"
+#include "DigiKeyboardPtBr.h"
 void setup() {
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(3000);
-  DigiKeyboard.print("Add-Type -AssemblyName System.speech");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.delay(100);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(100);
+  DigiKeyboardPtBr.println("powershell");
+  DigiKeyboardPtBr.delay(3000);
+  DigiKeyboardPtBr.println("Add-Type -AssemblyName System.speech");
+  DigiKeyboardPtBr.delay(100);
+  DigiKeyboardPtBr.println("$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer");
+  DigiKeyboardPtBr.delay(100);
   // Descomentar para usar uma voz feminina
-  //DigiKeyboard.print("$speak.SelectVoice('Microsoft Zira Desktop')");
-  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  //DigiKeyboard.delay(500);
-  DigiKeyboard.print("$speak.Speak(\"Here's a joke. Why do Java programmers wear glasses? Because they can't C#.\")");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("exit");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(KEY_N);
+  //DigiKeyboardPtBr.println("$speak.SelectVoice('Microsoft Zira Desktop')");
+  //DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.println("$speak.Speak(\"Here's a joke. Why do Java programmers wear glasses? Because they can't C#.\")");
+  DigiKeyboardPtBr.delay(100);
+  DigiKeyboardPtBr.print("exit");
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(100);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_N);
   for (;;) {
     /*empty*/
   }
